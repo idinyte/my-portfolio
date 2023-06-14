db553d3c32c449bd66feaa7ad7be4c2752118bb1
Idinyte <black.mad.eagle@gmail.com>
2023-06-14 18:01:44 +0300

    change contact page

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import '../styles/contact.css';
+import { SocialIcon } from 'react-social-icons';
 
 export const Contact = () => {
   return (
     <div id="contact_container">
       <h1>Contact me</h1>
-      <button id="email" onClick={() => window.open('mailto:rokasandrijauskas10.02@gmail.com')}><span className="material-symbols-outlined">
-        mail
-      </span>email
-      </button>
+      <SocialIcon url="https://github.com/idinyte" />
+      <SocialIcon url="mailto:rokasandrijauskas10.02@gmail.com" />
+      <SocialIcon url="https://www.linkedin.com/in/rokas-andrijauskas-a329a127a/" />
     </div >
   );
 }
